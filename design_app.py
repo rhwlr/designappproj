f02c8c37cc43dda8d180c0952e44ccf90310c576
rhwlr <rwlr167@gmail.com>
2021-11-20 09:38:16 +0000

    sample app

---
+++ design_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050, debug=True)
---
+++ design_app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp design_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" > tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  design_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "RUN python3 -m pip install flask" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/design_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t design_app .
+docker run -t -d -p 5050:5050 --name design_apprunning design_app
+docker ps -a
